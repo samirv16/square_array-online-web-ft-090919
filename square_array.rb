d6be36fc5ee153766c28b52dd6f6568913a498c9
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-11 18:14:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,11 @@ require "pry"
 
 def square_array(array)
   new_array = []
-  
+  index = 0
   
   array.each do |index|
     new_array << array[index] ** 2 
+    index += 1 
   end
   
   new_array
